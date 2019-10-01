3d09903f9b07ff122bf3d748f553a19581c3a1f8
Alfredo Diaz <mail4alfredo@gmail.com>
2019-10-01 16:14:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,25 @@
 require 'pry'
+class Song
 
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
+  attr_accessor :name
+  attr_reader :artist
+
+  @@songs = []
+
+  def self.all
+    @@songs
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self) unless artist.songs.include?(self)
+  end
+end
 # class Song
 #   attr_accessor :name
 #   attr_reader :artist
