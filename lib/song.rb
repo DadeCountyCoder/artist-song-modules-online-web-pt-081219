d2e1037dd596bafb888c32a1a90cddc0d9beb0da
Alfredo Diaz <mail4alfredo@gmail.com>
2019-10-01 16:13:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,32 +1,32 @@
 require 'pry'
 
-class Song
-  attr_accessor :name
-  attr_reader :artist
+# class Song
+#   attr_accessor :name
+#   attr_reader :artist
 
-  extend Memorable::ClassMethods
-  extend Findable::ClassMethods
-  include Memorable::InstanceMethods
-  include Paramable::InstanceMethods
+#   extend Memorable::ClassMethods
+#   extend Findable::ClassMethods
+#   include Memorable::InstanceMethods
+#   include Paramable::InstanceMethods
   
-  @@songs = []
+#   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+#   def initialize
+#     @@songs << self
+#   end
   
-  def initialize
-    @@artists << self
-    @songs = []
-  end
+#   def initialize
+#     @@artists << self
+#     @songs = []
+#   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+#   def self.find_by_name(name)
+#     @@songs.detect{|a| a.name == name}
+#   end
 
-  def self.all
-    @@songs
-  end
+#   def self.all
+#     @@songs
+#   end
 
   # def self.reset_all
   #   self.all.clear
@@ -36,11 +36,11 @@ class Song
   #   self.all.count
   # end
 
-  def artist=(artist)
-    @artist = artist
-  end
+#   def artist=(artist)
+#     @artist = artist
+#   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-end
+#   def to_param
+#     name.downcase.gsub(' ', '-')
+#   end
+# end
