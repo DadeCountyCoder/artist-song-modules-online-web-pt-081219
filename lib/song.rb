2cc78f9b7b0be5a79306acb819eb7e7a836004f8
Alfredo Diaz <mail4alfredo@gmail.com>
2019-10-01 16:08:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,10 @@ class Song
     @@songs << self
   end
   
-  
+  def initialize
+    @@artists << self
+    @songs = []
+  end
 
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
